d1eec2e589e05dabf9d0156e4b093da000d725de
Han <Han@Hannahs-MacBook-Pro.local>
2017-11-09 21:38:32 +0000

    javascript added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Page has loaded! I can't believe it!");
+
+function helloWorld() {
+    console.log('Hello World!');
+  }
+
+function buttonThree() {
+    document.getElementById('heading-two').innerHTML ='Hello World!';
+    console.log('button three pressed');
+}
